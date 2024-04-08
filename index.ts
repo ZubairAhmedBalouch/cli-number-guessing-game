4abf9c0aaa2bca2674d7a0213a313a91e3d49027
Zubair Ahmed Balouch <zubairahmedbalouch37@gmail.com>
2024-04-09 01:13:25 +0500

    added a message in console

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ import inquirer from "inquirer";
 // 3) Compare User Input with Computer Generated number and show result. -done
 
 const randomNumber = Math.floor(Math.random() *6 + 1); //1 to 6 numbers will show like ludo, it will generates number 1 to 6
-console.log("** Dear User Most Welcome to you in the Number Guessing Game **");
+console.log("**...Dear User Most Welcome to you in the Number Guessing Game...**");
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
